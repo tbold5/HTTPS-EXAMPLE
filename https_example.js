0d3aca19a5667d63eafd6d1e9dc006b941fff26a
Tuguldur <tuguldurbold5@gmail.com>
2018-09-30 20:44:03 +0000

    first change

---
+++ https_example.js
@@ -0,0 +1,24 @@
+var https = require('https');
+
+console.log('I did it!');
+
+var options = {
+    host: 'www.example.org',
+    path: '/'
+  };
+  
+  // called by https when the request is made.
+  var callback = function(response) {
+  console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
+}
+  
+  console.log("I'm about to make the request!");
+  
+  https.request(options, callback).end();
+  
+  console.log("I've made the request!");
